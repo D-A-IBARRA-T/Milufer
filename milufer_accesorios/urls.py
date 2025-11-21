d12e20b5a171fa7ea17ee039464c6af58378a9a1
JulianDM23 <juliandavidmt23@gmail.com>
2025-11-21 06:54:12 -0500

    feat: se agego login en django

--- milufer_accesorios/urls.py
+++ milufer_accesorios/urls.py
@@ -14,9 +14,26 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+# tu_proyecto/urls.py  (el del proyecto, no el de la app)
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth import views as auth_views
+from joyeria import views  # ← tu app
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # 1. Al abrir http://127.0.0.1:8000/ → aparece el login directamente
+    path('', auth_views.LoginView.as_view(
+        template_name='joyeria/login.html'
+    ), name='login'),
+
+    # 2. Después de iniciar sesión entra a la tienda real
+    path('inicio/', views.index, name='index'),
+
+    # 3. Logout → vuelve al login
+    path('logout/', auth_views.LogoutView.as_view(
+        next_page='/'
+    ), name='logout'),
 ]
